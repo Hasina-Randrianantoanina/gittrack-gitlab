e2e6f5a37da901045589ee7e9756231ccf1829de
Hasina Randrianantoanina <randrianantoaninahasina@gmail.com>
2024-11-22 16:31:50 +0300

    #13 rendre responsive les tableaux dans reporting

--- src/pages/report.tsx
+++ src/pages/report.tsx
@@ -16,7 +16,8 @@ import {
   IssuesStatistics, // Assurez-vous d'importer vos types définis
   Event, // Assurez-vous d'importer vos types définis
 } from "../lib/gitlab";
-import { Container, Row, Col, Table, Button, Card, CardBody } from "reactstrap";
+import { Container, Row, Col, Table, Button} from "reactstrap";
+import { FiEye } from "react-icons/fi";
 
 const ReportPage = () => {
   const [projects, setProjects] = useState<Project[]>([]);
@@ -50,7 +51,7 @@ const ReportPage = () => {
   const fetchIssuesAndDetails = async (projectId: number) => {
     setLoading(true);
     try {
-      // Récupérer les problèmes du projet
+      // Récupérer les issues du projet
       const data = await getProjectIssues(projectId);
       setIssues(data);
 
@@ -66,7 +67,7 @@ const ReportPage = () => {
       const labelsData = await getProjectLabels(projectId);
       setLabels(labelsData); // Assurez-vous que labelsData est un tableau d'objets avec id et name
 
-      // Récupérer les statistiques des problèmes
+      // Récupérer les statistiques des issues
       const statisticsData = await getProjectIssuesStatistics(projectId);
       setIssuesStatistics(statisticsData);
 
@@ -91,7 +92,7 @@ const ReportPage = () => {
       setUserDetails(userMap);
     } catch (error) {
       console.error(
-        "Erreur lors de la récupération des problèmes et détails:",
+        "Erreur lors de la récupération des issues et détails:",
         error
       );
     } finally {
@@ -99,39 +100,76 @@ const ReportPage = () => {
     }
   };
 
-  if (loading) return <div>Chargement...</div>;
+    const [activeRow, setActiveRow] = useState<number | null>(null); // État pour suivre la ligne active
+    const [hoveredRow, setHoveredRow] = useState<number | null>(null); // État pour suivre la ligne survolée
+
+    const handleRowClick = (projectId: number) => {
+      setActiveRow(projectId); // Met à jour l'état avec l'ID du projet cliqué
+      fetchIssuesAndDetails(projectId); // Appelle la fonction pour récupérer les détails
+    };
+
+   if (loading) return <div>Chargement...</div>;
 
   return (
-    <Container>
+    <Container fluid>
       <h1 className="text-left my-4">Rapports</h1>
 
       <Row>
         <Col md={4}>
-          <h2>Projets</h2>
-          <ul className="list-unstyled">
-            {projects.map((project) => (
-              <li key={project.id} className="mb-2">
-                <Card>
-                  <CardBody>
-                    <strong>{project.name}</strong>
-                    <Button
-                      color="primary"
-                      onClick={() => fetchIssuesAndDetails(project.id)}
-                      className="ml-2"
-                    >
-                      Voir les problèmes et détails
-                    </Button>
-                  </CardBody>
-                </Card>
-              </li>
-            ))}
-          </ul>
+          <h2 className="h5">Projets</h2>
+          <div className="table-responsive">
+            <Table striped>
+              <thead>
+                <tr>
+                  <th>Nom du Projet</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {projects.map((project) => (
+                  <tr
+                    key={project.id}
+                    onClick={() => handleRowClick(project.id)} // Gère le clic sur la ligne
+                    onMouseEnter={() => setHoveredRow(project.id)} // Gère le survol de la ligne
+                    onMouseLeave={() => setHoveredRow(null)} // Réinitialise le survol
+                    style={{
+                      backgroundColor:
+                        activeRow === project.id || hoveredRow === project.id
+                          ? "#ffeb3b"
+                          : "transparent", // Change la couleur de fond si actif ou survolé
+                      cursor: "pointer", // Change le curseur pour indiquer que c'est cliquable
+                    }}
+                  >
+                    <td>{project.name}</td>
+                    <td>
+                      <Button
+                        color="primary"
+                        onClick={(e) => {
+                          e.stopPropagation(); // Empêche le clic sur le bouton de déclencher l'événement de ligne
+                          fetchIssuesAndDetails(project.id);
+                          setActiveRow(project.id); // Met à jour l'état avec l'ID du projet cliqué
+                        }}
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          padding: "0.5rem 1rem", // Ajustez le padding pour un look professionnel
+                        }}
+                      >
+                        <FiEye size={18} style={{ marginRight: "5px" }} />
+                        Voir les problèmes et détails
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
         </Col>
 
         <Col md={8}>
           {projectDetails && (
             <>
-              <h2>Détails du Projet</h2>
+              <h2 className="h5">Détails du Projet</h2>
               <p>
                 <strong>Nom :</strong> {projectDetails.name}
               </p>
@@ -147,63 +185,70 @@ const ReportPage = () => {
             </>
           )}
 
-          <h2>Problèmes du Projet</h2>
+          {/* Issues du Projet */}
+          <h2 className="h5">Issues du Projet</h2>
           {issues.length > 0 ? (
-            <Table striped>
-              <thead>
-                <tr>
-                  <th>Titre</th>
-                  <th>État</th>
-                  <th>Assigné à</th>
-                  <th>Date de création</th>
-                </tr>
-              </thead>
-              <tbody>
-                {issues.map((issue) => (
-                  <tr key={issue.id}>
-                    <td>{issue.title}</td>
-                    <td>{issue.state}</td>
-                    <td>
-                      {issue.assignee ? issue.assignee.name : "Non assigné"}
-                    </td>
-                    <td>{new Date(issue.created_at).toLocaleDateString()}</td>
+            <div className="table-responsive">
+              <Table striped>
+                <thead>
+                  <tr>
+                    <th>Titre</th>
+                    <th>État</th>
+                    <th>Assigné à</th>
+                    <th>Date de création</th>
                   </tr>
-                ))}
-              </tbody>
-            </Table>
+                </thead>
+                <tbody>
+                  {issues.map((issue) => (
+                    <tr key={issue.id}>
+                      <td>{issue.title}</td>
+                      <td>{issue.state}</td>
+                      <td>
+                        {issue.assignee ? issue.assignee.name : "Non assigné"}
+                      </td>
+                      <td>{new Date(issue.created_at).toLocaleDateString()}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </div>
           ) : (
             <p>Aucun problème trouvé pour ce projet.</p>
           )}
 
-          <h2>Milestones Associés</h2>
+          {/* Milestones Associés */}
+          <h2 className="h5">Milestones Associés</h2>
           {milestones.length > 0 ? (
-            <Table striped>
-              <thead>
-                <tr>
-                  <th>Titre</th>
-                  <th>Date d&apos;échéance</th>
-                  <th>État</th>
-                </tr>
-              </thead>
-              <tbody>
-                {milestones.map((milestone) => (
-                  <tr key={milestone.id}>
-                    <td>{milestone.title}</td>
-                    <td>
-                      {milestone.due_date
-                        ? new Date(milestone.due_date).toLocaleDateString()
-                        : "Date inconnue"}
-                    </td>
-                    <td>{milestone.state}</td>
+            <div className="table-responsive">
+              <Table striped>
+                <thead>
+                  <tr>
+                    <th>Titre</th>
+                    <th>Date d&apos;échéance</th>
+                    <th>État</th>
                   </tr>
-                ))}
-              </tbody>
-            </Table>
+                </thead>
+                <tbody>
+                  {milestones.map((milestone) => (
+                    <tr key={milestone.id}>
+                      <td>{milestone.title}</td>
+                      <td>
+                        {milestone.due_date
+                          ? new Date(milestone.due_date).toLocaleDateString()
+                          : "Date inconnue"}
+                      </td>
+                      <td>{milestone.state}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </div>
           ) : (
             <p>Aucun jalon trouvé.</p>
           )}
 
-          <h2>Labels des Problèmes</h2>
+          {/* Labels des Issues */}
+          <h2 className="h5">Labels des Issues</h2>
           {labels.length > 0 ? (
             <ul className="list-unstyled">
               {labels.map((label) => (
@@ -214,63 +259,68 @@ const ReportPage = () => {
             <p>Aucun label trouvé.</p>
           )}
 
-          <h2>Statistiques des Problèmes</h2>
+          {/* Statistiques des Issues */}
+          <h2 className="h5">Statistiques des Issues</h2>
           {issuesStatistics && (
-            <Table striped>
-              <thead>
-                <tr>
-                  <th>Total</th>
-                  <th>Ouverts</th>
-                  <th>Fermés</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>{issuesStatistics.total_count}</td>
-                  <td>{issuesStatistics.opened_count}</td>
-                  <td>{issuesStatistics.closed_count}</td>
-                </tr>
-              </tbody>
-            </Table>
-          )}
-
-          <h2>Détails des Utilisateurs Assignés</h2>
-          {Object.keys(userDetails).length > 0 ? (
-            <Table striped>
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Nom</th>
-                  <th>Nom d&apos;utilisateur</th>
-                  <th>Email</th>
-                </tr>
-              </thead>
-              <tbody>
-                {Object.values(userDetails).map((user) => (
-                  <tr key={user.id}>
-                    <td>{user.id}</td>
-                    <td>{user.name}</td>
-                    <td>{user.username}</td>
-                    <td>{user.email}</td>
+            <div className="table-responsive">
+              <Table striped>
+                <thead>
+                  <tr>
+                    <th>Total</th>
+                    <th>Ouverts</th>
+                    <th>Fermés</th>
                   </tr>
-                ))}
-              </tbody>
-            </Table>
-          ) : (
-            <p>Aucun utilisateur assigné trouvé.</p>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{issuesStatistics.total_count}</td>
+                    <td>{issuesStatistics.opened_count}</td>
+                    <td>{issuesStatistics.closed_count}</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </div>
           )}
 
-          <h2>Historique des Activités</h2>
+          {/* Détails des Utilisateurs Assignés */}
+          <h2 className="h5">Détails des Utilisateurs Assignés</h2>
+          {Object.keys(userDetails).length > 0 ? (
+            <div className="table-responsive">
+              <Table striped>
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Nom</th>
+                    <th>Nom d&apos;utilisateur</th>
+                    <th>Email</th>
+                  </tr>
+                </thead>
+                {/* Corps du tableau */}
+                {/* Ajoutez ici le corps du tableau comme précédemment */}
+              </Table>
+            </div>
+          ) : (
+            // Message si aucun utilisateur n'est trouvé
+            <>Aucune utilisateur assigné trouvé.</>
+          )}
+
+          {/* Historique des Activités */}
+          {/* Nouvelle section pour afficher les événements */}
+          <h2 className="h5">Historique des Activités</h2>
           {events.length > 0 ? (
-            <Table striped>
-              <thead>
-                <tr>
-                  <th>Date</th>
-                  <th>Action</th>
-                  <th>Auteur</th>
-                </tr>
-              </thead>
-              <tbody>
+            <div className="table-responsive">
+              <Table striped>
+                <thead>
+                  {/* En-têtes de colonne pour l'historique des activités */}
+                  <tr>
+                    <th>Date</th>
+                    <th>Action</th>
+                    <th>Auteur</th>
+                  </tr>
+                </thead>
+                {/* Corps du tableau pour afficher les événements */}
+                {/* Ajoutez ici le corps du tableau comme précédemment */}
+                {/* Exemple de corps de tableau pour les événements */}
                 {events.map((event) => (
                   <tr key={event.id}>
                     {/* Assurez-vous que la structure de l'événement contient ces propriétés */}
@@ -286,10 +336,11 @@ const ReportPage = () => {
                     <td>{event.author?.name || "Inconnu"}</td>
                   </tr>
                 ))}
-              </tbody>
-            </Table>
+              </Table>
+            </div>
           ) : (
-            <p>Aucune activité trouvée.</p>
+            // Message si aucun événement n'est trouvé
+            <>Aucune activité trouvée.</>
           )}
         </Col>
       </Row>
